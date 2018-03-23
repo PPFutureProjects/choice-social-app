2fb3ed12b2c099e02a2d189244a0d52f3b1377b2
Serdar Ağlamış <serdaraglamis@gmail.com>
2018-03-23 22:40:21 +0100

    Updated env and added Turkish translations

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -6,19 +6,19 @@ export namespace Environment {
     menuType: 'overlay'
   };
   // Set language to use.
-  export const language = 'en';
+  export const language = 'tr';
   // Firebase Cloud Messaging Server Key.
   // Get your gcmKey on https://console.firebase.google.com, under Overview -> Project Settings -> Cloud Messaging.
   // This is needed to send push notifications.
   export const gcmKey = 'AAAAvE9kDCs:APA91bH6ZpM_eY5JTRSPJQ9GDCRGwiVz_kHIyjQv5yt5vb1IjaPO6E0MnYYPg-LYudhLFjtjhoaW3_UbN5m8Q7wACRqYMaGI_HguvwhXEUN0fCrk22zrwPC5TjsEIo4HRC6O9TGwp6Jf';
   // Set to your Firebase app, you can find your credentials on Firebase app console -> Add Web App.
   export const firebase = {
-    apiKey: "AIzaSyDmG7Pg6up_DpD-N-rRRXwdAosAaNzASwM",
-    authDomain: "firestarter-22aaf.firebaseapp.com",
-    databaseURL: "https://firestarter-22aaf.firebaseio.com",
-    projectId: "firestarter-22aaf",
-    storageBucket: "firestarter-22aaf.appspot.com",
-    messagingSenderId: "808785808427"
+    apiKey: "AIzaSyAkETAXT5cZ9WCxFLNEip4LwENZq49ihMY",
+    authDomain: "zigi-v2.firebaseapp.com",
+    databaseURL: "https://zigi-v2.firebaseio.com",
+    projectId: "zigi-v2",
+    storageBucket: "zigi-v2.appspot.com",
+    messagingSenderId: "434618250965"
   };
   // You can find your googleWebClientId on your Firebase app console -> Authentication -> Sign-in Method -> Google -> Web client ID
   export const googleWebClientId: string = '808785808427-75h0objp7eq3nehkr6mhin72glpr7sl6.apps.googleusercontent.com';
